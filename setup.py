bbb67005c5fb3d963989177fb83e0b445be553c1
franaln <franaln@gmai.com>
2015-04-13 22:26:31 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rootutils',
+    version='0.1',
+    description='',
+    url='https://github.com/franaln/rootutils',
+    author='Francisco Alonso',
+    author_email='franaln@gmail.com',
+    license='',
+    packages=['rootutils',],
+    scripts=['rootls', 'rootplot'],
+    zip_safe=False,
+)
